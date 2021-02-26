45fc7c701bde0f2775f3893ce55f23964a4ad110
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-26 09:02:54 -0600

    added comments

--- src/components/FeelingRating/FeelingRating.jsx
+++ src/components/FeelingRating/FeelingRating.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+/**
+ * FeelingRating Renders the First Form View for Feedback
+ * 
+ * Form validates that the proper data is sent to DB.
+ * Form dispatches input to the global state.
+ */
 function FeelingRating(){
   const dispatch = useDispatch();
 
   // local state for input
   const [feeling, setFeeling] = useState('');
   
-
+  // on FeelingRating form submission validate and dispatch appropriate data
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -17,12 +23,14 @@ function FeelingRating(){
       return alert('Please enter a number between 1 and 5 before submission.')
     }
     
-    // if there is data,
+    // if there is data, send local state to be stored in reducer
     dispatch({
       type: 'ADD_FEELING_RATING',
       payload: { feeling }
     })
 
+    // reset local state on submission
+    setFeeling('');
   }
 
   return(
@@ -30,6 +38,7 @@ function FeelingRating(){
       <h2>How are you feeling today?</h2>
       <form onSubmit={handleSubmit}>
         <input 
+          // forces the input value from string to number from submission
           onChange={event => setFeeling(Number(event.target.value))} 
           type="number" 
           placeholder="1-5" 

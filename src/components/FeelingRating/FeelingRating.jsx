fca104948fafb7725177c34c5a89d4db363540bc
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-26 08:45:18 -0600

    inputs is instantly converted to numbers

--- src/components/FeelingRating/FeelingRating.jsx
+++ src/components/FeelingRating/FeelingRating.jsx
@@ -12,7 +12,12 @@ function FeelingRating(){
     event.preventDefault();
 
     console.log('in handleSubmit, feeling is: ', feeling);
+    // validate data on form submission
+    if (feeling === ''){
+      return alert('Please enter a number between 1 and 5 before submission.')
+    }
     
+    // if there is data,
     dispatch({
       type: 'ADD_FEELING_RATING',
       payload: { feeling }
@@ -25,7 +30,7 @@ function FeelingRating(){
       <h2>How are you feeling today?</h2>
       <form onSubmit={handleSubmit}>
         <input 
-          onChange={event => setFeeling(event.target.value)} 
+          onChange={event => setFeeling(Number(event.target.value))} 
           type="number" 
           placeholder="1-5" 
           min = "1" 

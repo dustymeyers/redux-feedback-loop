14ed6c7d40865cf5208317540ca00d655999f2b7
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-27 14:04:07 -0600

    conditional rendering on home page for form submission

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,24 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home ({isFeedbackSubmitted, setIsFeedbackSubmitted}) {
+  console.log('isFeedbackSubmitted', isFeedbackSubmitted);
 
   return(
     <>
       <h2>Let us know how you're feeling this week!</h2>
-      <h4>Submit your feedback</h4>
+
+      {/* Home page set to render thank you message if isFeedbackSubmitted is true.
+          On successful feedback submission in ReviewFeedback.jsx, isFeedbackSubmitted will be set to true.
+          Default state set to render without thank you (false).*/}
+
+      {isFeedbackSubmitted 
+        ? <h3>Thanks for your submission! Would you like to submit your feedback again?</h3>
+        : <h4>Submit your feedback</h4>
+      }
+
       <div className="button">
-        <Link to="/question1">Give Feedback</Link>
+        {/* On form start, set isFeedbackSubmitted back to false to reset the state */}
+        <Link to="/question1" onClick={setIsFeedbackSubmitted(false)}>Give Feedback</Link>
       </div>
       
     </>

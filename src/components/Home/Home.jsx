233d85d718cdd1e51cb8d6955e364c35936ebfad
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-27 14:33:07 -0600

    readjusted rendering to make it more readable

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,22 +4,28 @@ import './Home.css';
 function Home ({isFeedbackSubmitted, setIsFeedbackSubmitted}) {
   console.log('isFeedbackSubmitted', isFeedbackSubmitted);
 
+  let homeMessage, buttonDisplay;
+  
+  
+  if (isFeedbackSubmitted === true) {
+    homeMessage = (<h3 className="successMessage">Thanks for your submission! Would you like to submit your feedback again?</h3>);
+    buttonDisplay = 'Give New Feedback';
+  } else {
+    homeMessage = (<h4>Submit your feedback</h4>);
+    buttonDisplay = 'Give Feedback';
+  }
+  
   return(
     <>
       <h2>Let us know how you're feeling this week!</h2>
 
-      {/* Home page set to render thank you message if isFeedbackSubmitted is true.
-          On successful feedback submission in ReviewFeedback.jsx, isFeedbackSubmitted will be set to true.
-          Default state set to render without thank you (false).*/}
-
-      {isFeedbackSubmitted 
-        ? <h3>Thanks for your submission! Would you like to submit your feedback again?</h3>
-        : <h4>Submit your feedback</h4>
-      }
+      
+      {homeMessage}
 
       <div className="button">
         {/* On form start, set isFeedbackSubmitted back to false to reset the state */}
-        <Link to="/question1" onClick={setIsFeedbackSubmitted(false)}>Give Feedback</Link>
+        <Link to="/question1">{buttonDisplay}</Link>
+  
       </div>
       
     </>
@@ -27,3 +33,12 @@ function Home ({isFeedbackSubmitted, setIsFeedbackSubmitted}) {
 } // end Home
 
 export default Home;
+
+// {/* Home page set to render thank you message if isFeedbackSubmitted is true.
+//           On successful feedback submission in ReviewFeedback.jsx, isFeedbackSubmitted will be set to true.
+//           Default state set to render without thank you (false).*/}
+
+//           {isFeedbackSubmitted 
+//             ? <h3>Thanks for your submission! Would you like to submit your feedback again?</h3>
+//             : <h4>Submit your feedback</h4>
+//           }
